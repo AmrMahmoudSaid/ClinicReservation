b68625e613ca3a5b1b17afe45ad8fc1d4c8839c1
amr mahmoud <amrmahmoud1900@gmail.com>
2023-12-20 07:17:45 +0200

    .

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
-FROM python:3.8
-
+FROM python:3
+ENV MONGO_URL mongodb://mongodb_container:27017
+ENV DB_NAME clinic_reservation
 WORKDIR /app
+RUN pip install --upgrade pip
 COPY requiremtns.txt .
 RUN pip install -r requiremtns.txt
 COPY . .
--- db_connection.py
+++ db_connection.py
@@ -1,6 +1,10 @@
+import os
+
 import pymongo
+from decouple import config
 
 url = 'mongodb+srv://AmrMahmoud:amr.mahmoud@cluster0.exwcjr4.mongodb.net/?retryWrites=true&w=majority'
+# url = os.environ.get('MONGO_URL')
 
 client = pymongo.MongoClient(url)
 
